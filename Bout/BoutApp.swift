e34808e5f4c35656db82f3d8b9e3456f4ecf61ca
Erica Stevens <ericastevens99@gmail.com>
2022-08-23 23:35:34 -0400

    Initial Commit

---
+++ Bout/BoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoutApp.swift
+//  Bout
+//
+//  Created by Erica Stevens on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
